82fb0105eae25dc24d62bad83a2aa3de7f7f4757
pooja03012005 <poojamadiwalar449@gmail.com>
2025-04-15 15:05:01 +0530

    hello

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+
+class Calculator {
+
+    public static void main(String[] args) {
+        int a = 10, b = 2;
+        System.out.println("Addtion of two numbers are:" + (a + b));
+        System.out.println("Substraction of two numbers are:" + (a - b));
+        System.out.println("Multiplication of two numbers are:" + (a * b));
+        System.out.println("division of two numbers are:" + (a / b));
+        System.out.println("modulus of two numbers are:" + (a % b));
+    }
+}
